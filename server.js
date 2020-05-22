4e879280065ecfdb1b567982fc361f980cc266d4
james-conant <jconant1993@gmail.com>
2020-05-21 23:21:47 -0500

    env setup

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("api Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`shits up on port ${PORT}!`));
