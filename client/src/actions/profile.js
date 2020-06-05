f7b626f4ade32cf92b03b4ebf49747d4590b61a0
james-conant <jconant1993@gmail.com>
2020-06-05 12:05:39 -0500

    Created action to create profile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-// import { setAlert } from ".alert";
+import { setAlert } from "./alert";
 
 import { GET_PROFILE, PROFILE_ERROR } from "./types";
 
@@ -13,6 +13,41 @@ export const getCurrentProfile = () => async (dispatch) => {
       payload: res.data,
     });
   } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Create or update profile
+export const createProfile = (formData, history, edit = false) => async (
+  dispatch
+) => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const res = await axios.post("/api/profile", formData, config);
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    });
+
+    dispatch(setAlert(edit ? "Profile updated" : "Profile Created", "success"));
+
+    if (!edit) {
+      history.push("/dashboard");
+    }
+  } catch (err) {
+    const errors = err.response.data.errors;
+    // console.log(errors);
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, "danger", 4000)));
+    }
     dispatch({
       type: PROFILE_ERROR,
       payload: { msg: err.response.statusText, status: err.response.status },
