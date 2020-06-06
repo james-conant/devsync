2e7523d564af2e3bfbfa242d4620e0826db09672
james-conant <jconant1993@gmail.com>
2020-06-05 19:58:30 -0500

    Created Education & Experience components to render in Dashboard component.

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getCurrentProfile } from "../../actions/profile";
+import Education from "./Education";
+import Experience from "./Experience";
 import DashboardActions from "./DashboardActions";
 import Spinner from "../layout/Spinner";
 
@@ -26,6 +28,8 @@ const Dashboard = ({
       {profile !== null ? (
         <Fragment>
           <DashboardActions />
+          <Experience experience={profile.experience} />
+          <Education education={profile.education} />
         </Fragment>
       ) : (
         <Fragment>
