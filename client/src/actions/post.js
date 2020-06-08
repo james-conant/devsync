aaf47034618d89997dbe39d8cf4028ba14e5eedc
james-conant <jconant1993@gmail.com>
2020-06-07 22:46:12 -0500

    Created deletePost action & reducer

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -2,7 +2,7 @@ import React from "react";
 import axios from "axios";
 import { setAlert } from "./alert";
 import PropTypes from "prop-types";
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from "./types";
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from "./types";
 
 // Get posts
 export const getPosts = () => async (dispatch) => {
@@ -46,6 +46,25 @@ export const removeLike = (id) => async (dispatch) => {
       type: UPDATE_LIKES,
       payload: { id, likes: res.data },
     });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// delete post
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    const res = await axios.delete(`api/posts/${id}`);
+
+    dispatch({
+      type: DELETE_POST,
+      payload: id,
+    });
+
+    dispatch(setAlert("Post removed", "success"));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
